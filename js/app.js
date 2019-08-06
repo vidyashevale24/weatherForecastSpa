696b320f4e11c95ab5cd1eca16f02c765a1b1cc0
vidyashevale24 <vidya.shevale24@gmail.com>
2019-08-06 11:19:21 +0530

    Resource implementation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var myApp = angular.module('myApp',['ngRoute','ngResource']);
 //Routes
-myApp.config(['$routeProvider','$locationProvider','$sceDelegateProvider', function($routeProvider,$locationProvider,$sceDelegateProvider) {
-    $locationProvider.hashPrefix('');
-	$routeProvider
+myApp.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+ $locationProvider.hashPrefix('');
+    $routeProvider
 	.when('/',{
 		templateUrl:'pages/home.html',
 		controller:'homeController'
@@ -13,27 +13,28 @@ myApp.config(['$routeProvider','$locationProvider','$sceDelegateProvider', funct
 	})
 }]);
 
+/*  myApp.filter('trustUrl', function ($sce) {
+    return function(url) {
+      return $sce.trustAsResourceUrl(url);
+    };
+  });*/
 
 //Services
 myApp.service('cityService',function(){
-	this.city = "New York , NY";
+	this.city = "München,DE";
 	
 })
 //Controllers
 myApp.controller('homeController',['$scope','cityService',function($scope,cityService){
 	$scope.city = cityService.city;
-	$scope.$watch('city',function(){
-		 cityService.city = $scope.city;
-	})
+	$scope.$watch('city',function(){  cityService.city = $scope.city; });
 }]);
 
 myApp.controller('forecastController',['$scope','$resource','cityService',function($scope,$resource,cityService){
 	$scope.city 		= cityService.city;
 	$scope.weatherAPI 	= $resource("https://samples.openweathermap.org/data/2.5/forecast/daily",{
-		callback:"JSON_CALLBACK"},
-								{  get: { method: "JSONP" } });
-	$scope.weatherRes 	= $scope.weatherAPI.get({ q :$scope.city,	cnt:2 });
-	
+		callback:"JSON_CALLBACK"},{  get: { method: "JSONP" } });
+	$scope.weatherRes 	= $scope.weatherAPI.get({q:$scope.city ,cnt:2});
 	console.log($scope.weatherRes);
 }]);
 
