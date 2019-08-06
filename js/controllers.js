80fee4fe6b516fb2360587f4bd6705062a4d541c
vidyashevale24 <vidya.shevale24@gmail.com>
2019-08-06 16:31:33 +0530

    clean up code

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,32 @@
-myApp.controller('homeController',['$scope','cityService',function($scope,cityService){
+myApp.controller('homeController',['$scope','cityService','$location',function($scope,cityService,$location){
 	$scope.city = cityService.city;
 	$scope.$watch('city',function(){  cityService.city = $scope.city; });
+	
+	$scope.submit = function(){
+		$location.path("/forecast")
+	}
 }]);
 
-myApp.controller('forecastController',['$scope','$resource','cityService','$http','$routeParams',function($scope,$resource,cityService,$http,$routeParams){
+myApp.controller('forecastController',['$scope','cityService','weatherService','$http','$routeParams',function($scope,cityService,weatherService,$http,$routeParams){
 	$scope.city = cityService.city;
 	$scope.days = $routeParams.days || 2;
 	
+	
 	//Tutorial solution
+	//Trial 1
 	/*$scope.weatherAPI 	= $resource("https://samples.openweathermap.org/data/2.5/forecast/daily",{
 		callback:"JSON_CALLBACK"},{  get: { method: "JSONP" } });
 	$scope.weatherRes 	= $scope.weatherAPI.get({q:$scope.city ,cnt:2});
 	console.log($scope.weatherRes);*/
 	
+	//Trial 2
 	/*$scope.weatherAPI 	= $resource("http://localhost:81/VS/angularJS/forecastApi.php",{
 	callback:"JSON_CALLBACK"},{  get: { method: "JSONP" } });
 	$scope.weatherRes 	= $scope.weatherAPI.get({q:$scope.city ,cnt:2});
 	console.log($scope.weatherRes);*/
+	
+	
+	//$scope.weatherRes 	= weatherService.getWeather($scope.city,$scope.days);
 	
 	$http({
 		method: 'GET',
--- js/services.js
+++ js/services.js
@@ -2,3 +2,11 @@ myApp.service('cityService',function(){
 	this.city = "Altstadt,DE";
 	
 });
+
+myApp.service('weatherService',['$resource',function($resource){
+	/*this.GetWeather  = function(city,days){
+		var weatherAPI 	= $resource("https://samples.openweathermap.org/data/2.5/forecast/daily",{
+		callback:"JSON_CALLBACK"},{  get: { method: "JSONP" } });
+		return weatherAPI.get({ q:city , cnt:days})
+	}*/
+}]);
